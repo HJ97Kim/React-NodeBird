d6a56cc50c3121654c6e9d600e53703cdc235813
Hyeongjin Kim <kimjin5537@daum.net>
2021-01-18 12:16:48 +0900

    Redux 설치

--- prepare/front/pages/_app.js
+++ prepare/front/pages/_app.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import Head from 'next/head';
 import 'antd/dist/antd.css';
 
+import wrapper from '../store/configureStore';
+
 const NodeBird = ({ Component }) => {
   return (
     <>
@@ -12,11 +14,11 @@ const NodeBird = ({ Component }) => {
       </Head>
       <Component />
     </>
-  )
+  );
 };
 
 NodeBird.propTypes = {
   Component: PropTypes.elementType.isRequired,
 }
 
-export default NodeBird;
+export default wrapper.withRedux(NodeBird);
